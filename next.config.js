60757d27648b1d67ead585c66ddf1e438454c0cf
Naoto Yamada <info@mosva.jp>
2026-02-10 11:43:37 +0900

    Update Next.js and TypeScript configs

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     domains: ['images.unsplash.com', 's3.us-west-2.amazonaws.com'],
     unoptimized: false,
   },
+  compiler: {
+    removeConsole: false,
+  },
+  swcMinify: true,
 }
 
 module.exports = nextConfig
